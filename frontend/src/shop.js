de97cb3ce1a22a1471d488e8ebcf298767eb04f3
Luca Dalmasso <lucadalmasso@gmail.com>
2020-12-07 13:43:14 +0100

    test

--- frontend/src/shop.js
+++ frontend/src/shop.js
@@ -3,8 +3,6 @@ import LoadingBox from "./components/loadingbox.js";
 import MessageBox from "./components/messagebox.js";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "./actions/productActions.js";
-import { addProductToCart } from "./actions/cartActions.js";
-import Select from "react-select";
 import Card from "./components/card.js"
 
 export default function Shop() {
@@ -31,3 +29,5 @@ export default function Shop() {
         </div>
     );
 }
+
+
