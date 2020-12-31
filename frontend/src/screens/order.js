c6661b34987b3d7b62c395e301a8507cfa89d878
Luca Dalmasso <lucadalmasso@gmail.com>
2020-12-31 12:17:24 +0100

    order overview screen completed (not styled, like the rest of the website)

--- frontend/src/screens/order.js
+++ frontend/src/screens/order.js
@@ -17,13 +17,8 @@ export default function Order(props) {
     console.log(orderId);
 
     const orderDetails = useSelector((state) => state.orderDetails);
-    // console.log("orderDetails", orderDetails);
     const { order, loading, error } = orderDetails;
-    console.log("order", order);
-    // console.log("order.ordine array", order.ordine);
-
-    /* let totalQty = 0;
-    order.ordine.map((singleProduct) => (totalQty += singleProduct.qty)); */
+    
 
     useEffect(() => {
         if (userInfo) {
@@ -34,82 +29,99 @@ export default function Order(props) {
         }
     }, [props.history, orderId, dispatch]);
 
-    /* return (
-        <h1>this is order</h1>
-    ) */
 
    return loading ? (
-        <LoadingBox></LoadingBox>
-    ) : error ? (
-        <MessageBox variant="danger">{error}</MessageBox>
-    ) : (
-        <>
-            {order && Object.keys(order).length && (
-                <div>
-                    <h1>Order {order._id}</h1>
-                    <div className="row">
-                        <div className="column top">
-                            <div className="card card-body">
-                                <h2>L'ordine contiene</h2>
-                                <ul>
-                                    {order &&
-                                        order.ordine.map((articolo) => (
-                                            <li>
-                                                <img
-                                                    alt={articolo.product.nome}
-                                                    src={articolo.product.foto}
-                                                ></img>
-                                                <p>
-                                                    Articolo:{" "}
-                                                    {articolo.product.nome}
-                                                </p>{" "}
-                                                <p>Quantità: {articolo.qty}</p>
-                                                <p>
-                                                    Prezzo:{" "}
-                                                    {articolo.product.prezzo}
-                                                </p>
-                                            </li>
-                                        ))}
-                                </ul>
-                            </div>
-                            <div className="card card-body">
-                                <h2>Dati per la spedizione</h2>
-                                <ul>
-                                    <li>
-                                        Nome:{" "}
+       <LoadingBox></LoadingBox>
+   ) : error ? (
+       <MessageBox variant="danger">{error}</MessageBox>
+   ) : (
+       <>
+           {order && Object.keys(order).length && (
+               <div>
+                   <h1>Order {order._id}</h1>
+                   <div className="row">
+                       <div className="column top">
+                           <div className="card card-body">
+                               <h2>L'ordine contiene</h2>
+                               <ul>
+                                   {order &&
+                                       order.ordine.map((articolo) => (
+                                           <div
+                                               className="row"
+                                               key={articolo.product._id}
+                                           >
+                                               <div>
+                                                   <img
+                                                       className="prod-image-small"
+                                                       alt={
+                                                           articolo.product.nome
+                                                       }
+                                                       src={`
+                                                           .${articolo.product.foto
+                                                           }`}
+                                                   ></img>
+                                               </div>
+                                               <div>
+                                                   <strong>Articolo: </strong>
+                                                   {articolo.product.nome}
+                                               </div>
+                                               <div>
+                                                   <strong>Quantità: </strong>
+                                                   {articolo.qty}
+                                               </div>
+
+                                               <div>
+                                                   <strong>Prezzo: </strong>
+                                                   {articolo.product.prezzo}
+                                               </div>
+                                           </div>
+                                       ))}
+                               </ul>
+                           </div>
+                           <div className="card card-body">
+                               <h2>Dati per la spedizione</h2>
+                               <p>
+                                        <strong>Nome:</strong>{" "}
                                         {order.spedizione.shippingFirstName}
-                                    </li>
-                                    <li>
-                                        Cognome:{" "}
+                                        <br />
+                                        <strong>Cognome:</strong>{" "}
                                         {order.spedizione.shippingLastName}
-                                    </li>
-                                    <li>
-                                        Indirizzo:{" "}
+                                        <br />
+                                        <strong>Indirizzo:</strong>{" "}
                                         {order.spedizione.shippingAddress}
-                                    </li>
-                                    <li>
-                                        Città: {order.spedizione.shippingCity}
-                                    </li>
-                                    <li>CAP: {order.spedizione.shippingCAP}</li>
-                                </ul>
-                            </div>
-                            <div className="card card-body">
-                                <h2>Metodo di pagamento</h2>
-                                <p>{order.metodoPagamento}</p>
-                            </div>
-                        </div>
-                        <div className="column top">
-                            <li>
-                                Subtotale({} articoli): €{" "}
-                                {order.subtotale}
-                            </li>
-                            <li>Costi spedizione: € {order.costiSpedizione}</li>
-                            <li>Totale: € {order.totale}</li>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </>
-    );
+                                        <br />
+                                        <strong>Comune:</strong> {order.spedizione.shippingCity}
+                                        <br />
+                                        <strong>CAP:</strong> {order.spedizione.shippingCAP}
+                                        <br />
+                                    </p>
+                           </div>
+                           <div className="card card-body">
+                               <h2>Metodo di pagamento</h2>
+                               <p>{order.metodoPagamento}</p>
+                           </div>
+                       </div>
+                       <div className="column top">
+                           <div className="card card-body">
+                               <li>
+                               Subtotale (
+                                    {order.ordine.reduce((a, c) => a + c.qty, 0)}{" "}
+                                    articoli): €{" "}
+                                    {order.ordine.reduce(
+                                        (a, c) => a + c.product.prezzo * c.qty,
+                                        0
+                                    )}
+                               </li>
+                               <li>
+                                   Costi spedizione: € {order.costiSpedizione}
+                               </li>
+                               <li>Totale: € {order.totale}</li>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           )}
+       </>
+   );
 }
  
